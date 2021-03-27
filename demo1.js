60fd6bab7cafd65017420a074857494d9e1038f0
akanksha-deshmukh <81348503+akanksha-deshmukh@users.noreply.github.com>
2021-03-27 13:09:39 +0530

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,10 @@
+var ele = document.getElementsByTagName('h1');
+console.log(ele[0]);
+document.getElementsByTagName('p');
+
+var e = document.getElementsByClassName('abc')
+console.log(e);
+
+var a = document.getElementById('no');
+console.log(a);
+
